15505f2284729bb5220b0041f7d9d2f35aeee5b2
Hrishi111297 <hrishi111297@gmail.com>
2024-04-24 15:26:49 +0530

    main file modified

--- BasicJavaCode.java
+++ BasicJavaCode.java
@@ -1,5 +1,7 @@
 public class BasicJavaCode{
 public static void main (String a[]){
 System.out.println("Hello Hrishikesh This is your first java code");
+System.out.println("Hello You are talented");
+
 }
 }
